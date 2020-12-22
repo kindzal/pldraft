4afee76f536e189c8de15b38e66165e02406d0d9
Viktor Persson <viktor.persson@arcsin.se>
2020-12-22 15:02:34 +0100

    Build site

---
+++ docs/_dist_/App.js
@@ -0,0 +1,11 @@
+import React from "../web_modules/react.js";
+import "./App.css.proxy.js";
+import SampleBoard from "./SampleBoard.js";
+
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "app"
+  }, /*#__PURE__*/React.createElement("section", null, /*#__PURE__*/React.createElement(SampleBoard, null)));
+}
+
+export default App;
