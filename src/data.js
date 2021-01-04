d41ce602b0546479bdafe88843ba9d59f7c22cfa
Bogusz <bogusz.dybowski@gmail.com>
2021-01-04 13:57:22 +0000

    changes for PL Draft 2021
    Signed-off-by: Bogusz <bogusz.dybowski@gmail.com>

--- src/data.js
+++ src/data.js
@@ -1,59 +1,129 @@
-export const pickOrder = [0, 1, 2, 3, 2, 3, 1, 0, 3, 2, 3, 2, 0, 1, 0, 1, 2, 3];
-
+export const pickOrder = [0, 1, 2, 3, 2, 3, 1, 0, 1, 0, 3, 2, 3, 2, 0, 1, 0, 1, 2, 3];
 export const teams = {
-  Goniec: {
-    id: "Goniec",
-    name: "Team Goniec",
-    playerNames: ["Goniec"],
-    theme: "Goniec",
-    sortOrder: 1,
-  },
-  Tom: {
-    id: "Tom",
-    name: "Team Tom",
-    playerNames: ["Tom"],
-    theme: "Tom",
-    sortOrder: 2,
-  },
-  Er: {
-    id: "Er",
-    name: "Team Er",
-    playerNames: ["Er"],
-    theme: "Er",
-    sortOrder: 3,
-  },
-  Insane: {
-    id: "Insane",
-    name: "Team Insane",
-    playerNames: ["Insane"],
-    theme: "Insane",
-    sortOrder: 4,
-  },
+    Goniec: {
+        id: "Goniec",
+        name: "Żołnierze Gońca",
+        playerNames: ["Goniec"],
+        theme: "Goniec",
+        sortOrder: 1
+    },
+    Tom: {
+        id: "Tom",
+        name: "TOMAHAWK",
+        playerNames: ["Tom"],
+        theme: "Tom",
+        sortOrder: 2
+    },
+    Er: {
+        id: "Er",
+        name: "REvenge",
+        playerNames: ["Er"],
+        theme: "Er",
+        sortOrder: 3
+    },
+    Insane: {
+        id: "Insane",
+        name: "LOCKdown",
+        playerNames: ["Insane"],
+        theme: "Insane",
+        sortOrder: 4
+    }
 };
-
 export const players = {
-  Adko: { name: "Adko", country_code: "pl" },
-  Blassy: { name: "Blassy", country_code: "pl" },
-  Chironex: { name: "Chironex", country_code: "pl" },
-  Doomie: { name: "Doomie", country_code: "pl" },
-  "Dr koks": { name: "Dr koks", country_code: "pl" },
-  Er: { name: "Er", country_code: "pl" },
-  Glinski: { name: "Glinski", country_code: "pl" },
-  Goniec: { name: "Goniec", country_code: "pl" },
-  Grl: { name: "Grl", country_code: "pl" },
-  Insane: { name: "Insane", country_code: "pl" },
-  Kat: { name: "Kat", country_code: "pl" },
-  Kornih: { name: "Kornih", country_code: "pl" },
-  Lolek: { name: "Lolek", country_code: "pl" },
-  Macler: { name: "Macler", country_code: "pl" },
-  Majq: { name: "Majq", country_code: "pl" },
-  Maks: { name: "Maks", country_code: "pl" },
-  Pooll: { name: "Pooll", country_code: "pl" },
-  Riki: { name: "Riki", country_code: "pl" },
-  Samon: { name: "Samon", country_code: "pl" },
-  Sebi: { name: "Sebi", country_code: "pl" },
-  Shamoth: { name: "Shamoth", country_code: "pl" },
-  Thunder: { name: "Thunder", country_code: "pl" },
-  Tiall: { name: "Tiall", country_code: "pl" },
-  Tom: { name: "Tom", country_code: "pl" },
+    Adko: {
+        name: "Adko",
+        country_code: "pl"
+    },
+    Blassy: {
+        name: "Blassy",
+        country_code: "pl"
+    },
+    Chironex: {
+        name: "Chironex",
+        country_code: "pl"
+    },
+    Doomie: {
+        name: "Doomie",
+        country_code: "pl"
+    },
+    "Dr koks": {
+        name: "Dr koks",
+        country_code: "pl"
+    },
+    Er: {
+        name: "Er",
+        country_code: "pl"
+    },
+    Glinski: {
+        name: "Glinski",
+        country_code: "pl"
+    },
+    Goniec: {
+        name: "Goniec",
+        country_code: "pl"
+    },
+    Grl: {
+        name: "Grl",
+        country_code: "pl"
+    },
+    Insane: {
+        name: "Insane",
+        country_code: "pl"
+    },
+    Kat: {
+        name: "Kat",
+        country_code: "pl"
+    },
+    Kornih: {
+        name: "Kornih",
+        country_code: "pl"
+    },
+    Lolek: {
+        name: "Lolek",
+        country_code: "pl"
+    },
+    Macler: {
+        name: "Macler",
+        country_code: "pl"
+    },
+    Majq: {
+        name: "Majq",
+        country_code: "pl"
+    },
+    Maks: {
+        name: "Maks",
+        country_code: "pl"
+    },
+    Pooll: {
+        name: "Pooll",
+        country_code: "pl"
+    },
+    Riki: {
+        name: "Riki",
+        country_code: "pl"
+    },
+    Samon: {
+        name: "Samon",
+        country_code: "pl"
+    },
+    Sebi: {
+        name: "Sebi",
+        country_code: "pl"
+    },
+    Shamoth: {
+        name: "Shamoth",
+        country_code: "pl"
+    },
+    Thunder: {
+        name: "Thunder",
+        country_code: "pl"
+    },
+    Tiall: {
+        name: "Tiall",
+        country_code: "pl"
+    },
+    Tom: {
+        name: "Tom",
+        country_code: "pl"
+    }
 };
